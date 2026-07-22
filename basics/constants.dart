85f030a83fe946edd04fe5bd84c490d7cef4848c
Idine <chamsidinebacar3@gmail.com>
2026-07-22 09:38:04 +0300

    Add basics example: var, final, const

---
+++ basics/constants.dart
@@ -0,0 +1,7 @@
+void main() {
+  var name = "Idine"; // var it can change
+  final age = "22"; // final or const cannot change [ runtime constant ]
+  double pi = 3.14; // compile-time constant
+
+  print("$name\n$age\n$pi");
+}
