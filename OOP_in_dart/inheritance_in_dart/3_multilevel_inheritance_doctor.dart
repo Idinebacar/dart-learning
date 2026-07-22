855c810b4f537379025957a2f84226dc3f2cc4bf
Idine <chamsidinebacar3@gmail.com>
2026-07-22 09:57:10 +0300

    Update OOP example: multilevel inheritance with Doctor

--- OOP_in_dart/inheritance_in_dart/3_multilevel_inheritance_doctor.dart
+++ OOP_in_dart/inheritance_in_dart/3_multilevel_inheritance_doctor.dart
@@ -1,33 +1,32 @@
 class Person {
-    String? name;
-    String? age;
-
+  String? name;
+  int? age;
 }
+
 // child class inheriting from person
 class Doctor extends Person {
-    List<String>? degrees;    // additional properties specific to doctor
-    String? hospitalName;
+  List<String>? degrees; // additional properties specific to doctor
+  String? hospitalName;
 
-    // method
-    void display() {
-        print("Name: $name");
-        print("Age: $age");
-        print("Degrees: $degrees");
-        print("Hospital: $hospitalName");
-    }
+  // method
+  void display() {
+    print("Name: $name");
+    print("Age: $age");
+    print("Degrees: $degrees");
+    print("Hospital: $hospitalName");
+  }
 }
 
 // child class inheriting from doctor
 class Specialist extends Doctor {
-    String? specialization;
+  String? specialization;
 
-    @override
-    void display() {
-
-        // calls the display() methood from the doctor
-        super.display();
-        print("Speccialization: $specialization");
-    }
+  @override
+  void display() {
+    // calls the display() methood from the doctor
+    super.display();
+    print("Speccialization: $specialization");
+  }
 }
 
 void main() {
