6248c91dccb719f6a6b9962aa69af6327aaa76db
Idine <chamsidinebacar3@gmail.com>
2026-07-22 09:58:43 +0300

    Update user input example: simple calculator

--- user_input_in_Dart/simple_calculator_input.dart
+++ user_input_in_Dart/simple_calculator_input.dart
@@ -1,12 +1,12 @@
 import "dart:io";
 
-void main(){
-    print("Enter the first number: ")
-    int num1 = int.parse(stdin.readLineSync()!);
+void main() {
+  print("Enter the first number: ");
+  int num1 = int.parse(stdin.readLineSync()!);
 
-    print("Enter the second number: ");
-    int num2 = int.parse(stdin.readLineSync()!);
+  print("Enter the second number: ");
+  int num2 = int.parse(stdin.readLineSync()!);
 
-    int sum = num1 + num2;
-    print("Sum: $sum");
+  int sum = num1 + num2;
+  print("Sum: $sum");
 }
