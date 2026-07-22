485cc567c81af33df3a7b60e8577d41da5e91d9e
Idine <chamsidinebacar3@gmail.com>
2026-07-22 09:53:47 +0300

    Update condition example: greatest of three numbers

--- condition_and_Loop/conditions_in_dart/greatest_number.dart
+++ condition_and_Loop/conditions_in_dart/greatest_number.dart
@@ -1,18 +1,20 @@
 /* Program: greatest number
 
-    Purpose: 
-        This program compares three numbers 
-        and finds the greatest (largest) number among them.
-        It uses if conditions and the logical AND operator (&&) 
-        to check which number is bigger than the other two numbers.
+   Purpose:
+     This program compares three numbers 
+     and finds the greatest (largest) number among them.
 */
 
 void main() {
-    int num1 = 1200;
-    int num2 = 1000;
-    int num3 = 150;
+  int num1 = 1200;
+  int num2 = 1000;
+  int num3 = 150;
 
-    if (num1 > num2 && num1 > num3) {
-        print()
-    }
+  if (num1 > num2 && num1 > num3) {
+    print("Greatest number is: $num1");
+  } else if (num2 > num1 && num2 > num3) {
+    print("Greatest number is: $num2");
+  } else {
+    print("Greatest number is: $num3");
+  }
 }
