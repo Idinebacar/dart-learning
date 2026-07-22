33f67542eaadfc02cc3f2198c5df832801436c00
Idine <chamsidinebacar3@gmail.com>
2026-07-22 09:51:47 +0300

    Add user input example: number input and sum

---
+++ user_input_in_Dart/number_input.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  print("Enter first number:");
+  int num1 = int.parse(stdin.readLineSync()!);
+
+  print("Enter second number:");
+  int num2 = int.parse(stdin.readLineSync()!);
+
+  print("Sum: ${num1 + num2}");
+}
